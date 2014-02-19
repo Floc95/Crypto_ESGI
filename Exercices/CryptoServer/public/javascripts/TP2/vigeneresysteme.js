593c602c10b18186b6313bd2f1c9c7809a4ba081
Floc95 <florianconstant@gmail.com>
2014-02-19 15:30:05 +0100

    Ajout du TP2 + génération clé & encodage vigenere

---
+++ Exercices/CryptoServer/public/javascripts/TP2/vigeneresysteme.js
@@ -0,0 +1,54 @@
+function VigenereSysteme()
+{
+	var self = this;
+	self.key = null;
+	self.alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+	self.map = {};
+
+	for (var i = 0; i < self.alpha.length; i++) {
+		self.map[self.alpha[i]] = i;
+	};
+
+	self.getDecalAlphaOf = function(charact, decalage)
+	{
+		var index = decalage + self.map[charact];
+		if (index >= self.alpha.length)
+			index = index - self.alpha.length;
+		return self.alpha[index];
+	};
+
+	self.encode = function(source){
+		var result = "";
+		var keyIdx = 0;
+		for (var i = 0; i < source.length; i++) {
+			if (keyIdx >= self.key.length)
+				keyIdx = 0;
+			result += self.getDecalAlphaOf(source[i], self.map[self.key[keyIdx]]);
+			keyIdx++;
+		};
+		return result;
+	}
+
+	self.decode = function(source){
+		var result = "";
+		var keyIdx = 0;
+		for (var i = 0; i < source.length; i++) {
+			if (keyIdx >= self.key.length)
+				keyIdx = 0;
+			result += self.getDecalAlphaOf(source[i], self.map[self.key[keyIdx]]);
+			keyIdx++;
+		};
+		return result;
+	}
+
+	self.generateKey = function(length){
+		var keygen = new Array(length);
+
+		for (var i = 0; i < length; i++){
+			var random = Math.floor(Math.random()*26);
+			keygen[i] = String.fromCharCode(65 + random);
+		}
+
+		self.key = keygen;
+	}
+}
