52f0d05996da40e4acce532440cf8670cfc5773d
mperroteau <marine.perroteau@gmail.com>
2014-02-21 12:17:00 +0100

    Lecture + Ecriture dans un fichier
    Lecture du fichier et ecriture a la suite

--- Exercices/CryptoServer/server/user.js
+++ Exercices/CryptoServer/server/user.js
@@ -4,8 +4,9 @@ exports.addUser = function(user){
 	console.log('Entree dans addUser'.green);
 
 	var fs = require("fs");
-	var currentcontent = "";
-	fs.writeFileSync("./server/data/users.txt", user, "UTF-8");
+	var file = "./server/data/users.txt";
+	var currentcontent = fs.readFileSync(file,'utf8');
+	fs.writeFileSync(file, currentcontent + "\r\n" + user, "UTF-8");
 };
 	
 
