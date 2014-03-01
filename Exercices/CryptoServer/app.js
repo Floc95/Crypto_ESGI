03ce9e3cf459cb5e96502da57e9d856cfa2546eb
mperroteau <marine.perroteau@gmail.com>
2014-03-01 14:48:46 +0100

    Index automatique dans auto index

--- Exercices/CryptoServer/app.js
+++ Exercices/CryptoServer/app.js
@@ -1,7 +1,7 @@
 
-/**
- * Module dependencies.
- */
+    /**
+     * Module dependencies.
+     */
 
 var express = require('express'),
     http = require('http'),
@@ -10,39 +10,65 @@ var express = require('express'),
     app = express(),
     httpServer = http.createServer(app);
 
-//configuration du serveur (port, dossier affiché)
-app.configure(function () {
-    app.set('port', 3000);
-    app.use(express.static(__dirname + '/public'));
-    app.use(express.urlencoded());
+
+    /**
+     *  Elements de configuration du serveur
+     */
+
+    //configuration du serveur (port, dossier affiché)
+    app.configure(function () {
+        app.set('port', 3000);
+        app.use(express.static(__dirname + '/public'));
+        app.use(express.urlencoded());
 });
 
-//Affichage lors du lancement du serveur
-httpServer.listen(app.get('port'), function () {
+    //Affichage lors du lancement du serveur
+    httpServer.listen(app.get('port'), function () {
     console.log("Crypto server listening on port %s.", httpServer.address().port);
 });
 
-app.post('/', function(request, response){
-	console.log('Entree dans add.post'.green);
-	var name = request.body.user.name;
-	var mail = request.body.user.email;
-	/*
-    console.log(name.red);
-    console.log(mail.green);
-    response.redirect('');
-    */
-    var user = require('./server/user');
-    user.addUser(name + " " + mail);
-    response.redirect('');
-});
 
-app.post('/createFile', function(request, response){
-	var fileName = request.body.file.name;
-	require('./server/user').createDataUser(fileName);
-	response.redirect('');
-});
+    /**
+     *   Fonctions du serveur
+     */
 
-app.get('/autoindex', function(request, response){
-	console.log('Entree autoindex'.green);
-	response.end(require('./server/index').getLinks());
-});
+    app.post('/', function(request, response){
+        console.log('Entree dans add.post'.green);
+        var name = request.body.user.name;
+        var mail = request.body.user.email;
+        /*
+        console.log(name.red);
+        console.log(mail.green);
+        response.redirect('');
+        */
+        var user = require('./server/user');
+        user.addUser(name + " " + mail);
+        response.redirect('');
+    });
+
+
+    //Fonction permettant de créer un fichier en fonction d'éléments envoyés depuis un formulaire (/createFile)
+    app.post('/createFile', function(request, response){
+        var fileName = request.body.file.name;
+        require('./server/user').createDataUser(fileName);
+        response.redirect('');
+    });
+
+    //Fonction index permettant d'afficher tous les éléments présents dans le fichier view
+    app.get('/autoindex', function(request, response){
+        var links = "";
+
+        var directory = fs.readdirSync("./public/views");
+
+        for (i = 0; i < directory.length; i++) {
+            var filenames = fs.readdirSync("./public/views/"+directory[i]);
+            for (y = 0; y < filenames.length; y++)
+                links += '<a href="./views/'+directory[i]+'/'+filenames[y]+'">'+filenames[y]+'</a></br>';
+        }
+
+        response.end(links);
+    });
+
+    /*
+       Attention ! Bien spécifier dans l'utilisation des fonctions Sync (sinon asynchrone par défaut).
+     */
--- Exercices/CryptoServer/server/index.js
+++ Exercices/CryptoServer/server/index.js
@@ -1,11 +1,27 @@
-exports.getLinks = function() {
+exports.getLinks = function(callback) {
 	var fs = require('fs');
 	var indexcontent = "";
-	fs.readdir("./public/views",function(error,directoryObject){
-		for( var i in directoryObject){
-			//Scaner les fichiers dans le dossier courant
-			console.log(directoryObject[i]);
+	var tablefile = [];
+	
+	fs.readdir('./public/views',function(error,directoryObject){
+		for(var i in directoryObject){
+			var directory = directoryObject[i];
+			//console.log(directoryObject[i]);
+			fs.readdir('./public/views/'+ directory, function(error,fileObject){
+				for(var y in fileObject){
+					var link = directory.red + '/' + fileObject[y];
+					//console.log(directory.red + '/' + fileObject[y].green);
+					//indexcontent += '<a href="'+link+'">'+fileObject[y]+'</a></br>';
+                    callback('<a href="'+link+'">'+fileObject[y]+'</a></br>');
+					//console.log(indexcontent);
+				}
+			});
 		}
+		
 	});
-	return indexcontent;
+
+	//Stocker dans un tableau
+	console.log("contenu : "+indexcontent.blue);
+	//callback indexcontent;
+    callback(indexcontent);
 };
