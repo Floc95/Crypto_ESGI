ce786c7d686f69be0ac6d5fbcf69ffb337e24579
mperroteau <marine.perroteau@gmail.com>
2014-03-02 16:18:36 +0100

    Debut interface class Certificat

---
+++ Exercices/CryptoServer/server/certificat.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Marine on 02/03/14.
+ */
+function Certificat(){
+    //TO DO : classe permettant de générer et gérer les
+    var self = this;
+
+    self.createCertificat= function(){
+        //TO DO : création du certificat, par défaut, il n'est pas validé
+    };
+
+    self.validCertificat = function(){
+        //TO DO : Validation du certificat (variable passant de true a false dans le fichier JSON certificats.
+    };
+
+    self.generateKeys = function(){
+        //TO DO : Génération des clés pour le certificat
+    };
+
+};
+
+exports.Certificat = Certificat;
