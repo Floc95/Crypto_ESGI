8df06ef56bc054cf21c91fa6c411ce391f6a1e5c
mperroteau <marine.perroteau@gmail.com>
2014-03-07 11:43:57 +0100

    début interface classe Certificat

--- Projet/server/certificat.js
+++ Projet/server/certificat.js
@@ -5,18 +5,48 @@ function Certificat(){
     //TO DO : classe permettant de générer et gérer les
     var self = this;
 
+
+    /*
+        Génération d'une clé privée
+        Créer un fichier de demande de signature de certificat (CRS Certificate Signin Request)
+            Exemple ??? :
+            //a partir de la clé privée préalablement crée
+                Country Name (2 letter code) [AU]:FR
+                State or Province Name (full name) [Some-State]:CORSE
+                Locality Name (eg, city) []:Ajaccio
+                Organization Name (eg, company) [Internet Widgits Pty Ltd]:LLB
+                Organizational Unit Name (eg, section) []:BTSINFO
+                Common Name (eg, YOUR name) []:wiki.domain1.org
+                Email Address []:
+    */
+    //Création d'un nouveau formulaire
+    //Validation d'un formulaire
+        //Délivrance du certificat
+        
+
+    self.generatePrivateKey = function(){
+        //Génration de la clé privée
+    };
+
+    self.generatePublicKey = function(){
+
+    };
+
     self.createCertificat= function(){
         //TO DO : création du certificat, par défaut, il n'est pas validé
     };
 
-    self.validCertificat = function(){
+    self.validateCertificate = function(){
         //TO DO : Validation du certificat (variable passant de true a false dans le fichier JSON certificats.
     };
 
-    self.generateKeys = function(){
-        //TO DO : Génération des clés pour le certificat
+    self.saveCertificate = function(){
+
     };
 
+    self.validCertificat = function(){
+        
+    };
 };
 
 exports.Certificat = Certificat;
